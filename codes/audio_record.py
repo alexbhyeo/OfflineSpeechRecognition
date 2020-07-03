a07c74ec26aae2f3b3395179ab06294f2825d2d7
yeoboonhong <yeoboonhong@patsnap.com>
2020-07-03 10:47:39 +0800

    add audio recorder

---
+++ codes/audio_record.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul  1 17:39:11 2020
+
+@author: yeoboonhong
+"""
+
+import pyaudio # Soundcard audio I/O access library
+import wave # Python 3 module for reading / writing simple .wav files
+
+# Setup channel info
+FORMAT = pyaudio.paInt16 # data type formate
+CHANNELS = 2 # Adjust to your number of channels
+RATE = 44100 # Sample Rate
+CHUNK = 1024 # Block Size
+RECORD_SECONDS = 5 # Record time
+WAVE_OUTPUT_FILENAME = "good_record.wav"
+
+# Startup pyaudio instance
+audio = pyaudio.PyAudio()
+
+# start Recording
+stream = audio.open(format=FORMAT, channels=CHANNELS,
+                rate=RATE, input=True,
+                frames_per_buffer=CHUNK)
+print ("recording...")
+frames = []
+
+# Record for RECORD_SECONDS
+for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
+    data = stream.read(CHUNK)
+    frames.append(data)
+print ("finished recording")
+
+
+# Stop Recording
+stream.stop_stream()
+stream.close()
+audio.terminate()
+
+# Write your new .wav file with built in Python 3 Wave module
+waveFile = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
+waveFile.setnchannels(CHANNELS)
+waveFile.setsampwidth(audio.get_sample_size(FORMAT))
+waveFile.setframerate(RATE)
+waveFile.writeframes(b''.join(frames))
+waveFile.close()
+
+
+  
+  
